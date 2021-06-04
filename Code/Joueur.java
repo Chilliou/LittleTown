72e9eb160b1655725ca323790993f1023498b1ba
unknown <chilliou07@gmail.com>
2021-06-05 00:02:46 +0200

    Ajout Constructeur et majScore

--- Code/Joueur.java
+++ Code/Joueur.java
@@ -1,13 +1,22 @@
 public class Joueur
 {
-    // Attributs
-    private int iScore;
-    private String sCouleur;
-    private Tuile ensBatiment;
-    
-    
-    public Joueur()
-    {
+	// Attributs
+	private int iScore;
+	private String sCouleur;
+	private Tuile[] ensBatiment;
+	private Ouvrier[] ensOuvrier;
 
-    }
+
+	public Joueur(String sCouleur)
+	{
+		this.iScore=0;
+		this.sCouleur = sCouleur;
+		this.ensBatiment = new Tuile[5]; // Pour le futur c'est forcement le nombre de maison dispo  a la base
+		this.ensOuvrier = new Ouvrier[3]; // Depend toujours du nbJoueurs
+	}
+
+	public void majScore(int i)
+	{
+		this.iScore+=i;
+	}
 }
