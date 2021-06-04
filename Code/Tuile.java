e7573ca309b1351f303ea8d65f2107f9ff04741a
Matheo Meinerad <matheo.meinerad.pro@gmail.com>
2021-06-04 15:16:33 +0200

    Ajout class Tuile

---
+++ Code/Tuile.java
@@ -0,0 +1,16 @@
+public class Tuile
+{
+	
+	private String nom;
+	/*
+	private int cout;
+	private int CoutProduction
+	private int RevientProduction; A revoir pour les ressources ...
+	*/
+	private int score;
+	
+	public Tuile ()
+	{
+		
+	}
+}
