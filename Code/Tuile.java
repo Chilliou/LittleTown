01e98ecdc31e41c56ec3ae903d3c6ede15d52d19
Matheo Meinerad <matheo.meinerad.pro@gmail.com>
2021-06-04 15:46:36 +0200

    Modification Tuile.java

--- Code/Tuile.java
+++ Code/Tuile.java
@@ -1,16 +1,39 @@
+import java.util.ArrayList;
+
 public class Tuile
 {
 	
 	private String nom;
-	/*
-	private int cout;
-	private int CoutProduction
-	private int RevientProduction; A revoir pour les ressources ...
-	*/
+	
+	private ArrayList<Ressource> cout;
+	private ArrayList<Ressource> coutProduction;
+	private ArrayList<Ressource> revientProduction;
+	
 	private int score;
 	
-	public Tuile ()
+	public Tuile ( String nom, int score)
 	{
+		this.nom   = nom;
 		
+		this.cout              = new ArrayList ();
+		this.coutProduction    = new ArrayList ();
+		this.revientProduction = new ArrayList ();
+		
+		this.score = score;
+	}
+	
+	public boolean ajouterCout ( Ressource r )
+	{
+		this.cout.add ( r );
+	}
+	
+	public boolean ajouterCoutProd ( Ressource r )
+	{
+		this.coutProduction.add ( r );
+	}
+	
+	public boolean ajouterRevientProd ( Ressource r )
+	{
+		this.revientProduction.add ( r );
 	}
 }
