248d5c1159515d10cd430c71726a64ad3c23386a
Quentin <quentin.savean@gmail.com>
2021-06-10 16:23:14 +0200

    Ajout frame aide

---
+++ Code/IHM/FrameHelp.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+
+public class FrameHelp extends JFrame
+{
+
+	private PanelHelp panelHelp;
+
+    Controleur ctrl;
+
+	public FrameHelp ( Controleur ctrl )
+	{
+		this.ctrl = ctrl;
+
+		this.setTitle    ( "Aide" );
+		this.setSize(200, 200);
+        this.setResizable(false);
+
+		// Organisation du LayoutManager
+
+		// Création des Panels
+		this.panelHelp  = new PanelHelp ( ctrl );
+
+		// Positionnement des Panels
+		this.add ( this.panelHelp, BorderLayout.NORTH );
+
+		// Activation de la fenetre
+        this.setLocationRelativeTo(null);
+		this.setVisible ( true );
+	}
+	
+	public void majIHM()
+	{
+		this.panelHelp.repaint();
+	}
+
+
+}
---
+++ Code/IHM/PanelHelp.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+public class PanelHelp extends JPanel
+{
+
+    Controleur ctrl;
+
+    public PanelHelp(Controleur ctrl) 
+    {
+        this.ctrl = ctrl;
+        
+
+    }
+
+    
+}
