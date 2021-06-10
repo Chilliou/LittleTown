b3bd71129dd782b5938edf418d96b5e6e184f53a
chilliou <Chilliou07@gmail.com>
2021-06-10 12:22:36 +0200

    Enlevage des méthode getIhm -Fonctionne

--- Code/Metier.java
+++ Code/Metier.java
@@ -67,12 +67,12 @@ public class Metier
             
             try
             {
-              TuileVide terrain = this.ctrl.getIhm().getTuileVide( x, y );
+              TuileVide terrain = this.ctrl.getTuileVide( x, y );
     
               if ( terrain.getNom().equals( "Vide" ) && terrain.getOuvrier() == null )
               {
                 terrain.setOuvrier( new Ouvrier ( x, y, this.getJoueurActif()) );
-                this.ctrl.getIhm().setTuileVide ( x, y, terrain );
+                this.ctrl.setTuileVide ( x, y, terrain );
                 this.dernierOuvrierPos = sCoordOuvrier;
     
                 // Le joueur a fait son action
@@ -114,9 +114,9 @@ public class Metier
                 if ( lig == 6 && col == 1)
                     bat = new Batiment ( "Blé", "A1", "", "C1", 3 );
                 else
-                    bat = this.ctrl.getIhm().getBatiment( lig, col ) ;
+                    bat = this.ctrl.getBatiment( lig, col ) ;
 
-                TuileVide terrain = this.ctrl.getIhm().getTuileVide( x, y );
+                TuileVide terrain = this.ctrl.getTuileVide( x, y );
 
                 if(!terrain.getNom().equals( "Vide" ) )
                     return false;
@@ -125,17 +125,17 @@ public class Metier
                     if(this.tabJoueurs[numJoueurActif].getRsc(bat.getCout().charAt(i))<Integer.parseInt(bat.getCout().charAt(i+1)+""))
                         return false;
 
-                if ( bat.getNom().equals( "Blé" ) && this.ctrl.getIhm().enleverBle() )
+                if ( bat.getNom().equals( "Blé" ) && this.ctrl.enleverBle() )
 
 
                 //DU coup on peut ajouter le batiment 
                 for(int i=0;i<bat.getCout().length();i+=2)
                     this.tabJoueurs[numJoueurActif].echangerRscJoueurVBanque(this.banque,bat.getCout().charAt(i),Integer.parseInt(bat.getCout().charAt(i+1)+""));
 
-                this.ctrl.getIhm().setTuile(x,y,bat);
+                this.ctrl.setTuile(x,y,bat);
                 if ( ! bat.getNom().equals( "Blé" ) )
-                    this.ctrl.getIhm().setTuile(lig,col,terrain);
-                this.ctrl.getIhm().addOuvrier(this.numJoueurActif);
+                    this.ctrl.setTuile(lig,col,terrain);
+                this.ctrl.addOuvrier(this.numJoueurActif);
                 this.tabJoueurs[numJoueurActif].changeScore(bat.getScore());
                 bat.setProprietaire(this.tabJoueurs[numJoueurActif]);
                 this.tabJoueurs[numJoueurActif].setAction(true);
@@ -161,7 +161,7 @@ public class Metier
     public String getSaisie()
     {
       String saisie;
-      saisie = this.ctrl.getIhm().getSaisiePos();
+      saisie = this.ctrl.getSaisiePos();
       return saisie;
     }
 
@@ -185,24 +185,24 @@ public class Metier
             {
 
                 // Vérification que la tuile n'a pas deja était activée.
-                if (this.ctrl.getIhm().getTuile(xTuile, yTuile).isActivable())
+                if (this.ctrl.getTuile(xTuile, yTuile).isActivable())
                 {
 
-                    System.out.println("> Activation de la tuile : " + this.ctrl.getIhm().getTuile(xTuile, yTuile).getNom());
+                    System.out.println("> Activation de la tuile : " + this.ctrl.getTuile(xTuile, yTuile).getNom());
 
-                    if ( this.ctrl.getIhm().getBatiment(xTuile, yTuile).getProprietaire() != this.tabJoueurs[this.numJoueurActif] )
+                    if ( this.ctrl.getBatiment(xTuile, yTuile).getProprietaire() != this.tabJoueurs[this.numJoueurActif] )
                     {
                         if ( this.tabJoueurs[this.numJoueurActif].getRsc( 'M' ) > 0 )
-                            this.tabJoueurs[this.numJoueurActif].echangerRscJoueurVBanque ( this.ctrl.getIhm().getBatiment(xTuile, yTuile).getProprietaire(), 'M', 1 );
+                            this.tabJoueurs[this.numJoueurActif].echangerRscJoueurVBanque ( this.ctrl.getBatiment(xTuile, yTuile).getProprietaire(), 'M', 1 );
                         else
                             return;
 
                     }
 
                     this.banque.echangerRscBanqueVJoueur(this.tabJoueurs[numJoueurActif],
-                                                         this.ctrl.getIhm().getBatiment(xTuile, yTuile).getRevientProd().charAt(0),
-                                                         Integer.parseInt( this.ctrl.getIhm().getBatiment(xTuile, yTuile).getRevientProd().charAt(1) + "" ) );
-                    this.ctrl.getIhm().getTuile(xTuile, yTuile).setActivation(false);
+                                                         this.ctrl.getBatiment(xTuile, yTuile).getRevientProd().charAt(0),
+                                                         Integer.parseInt( this.ctrl.getBatiment(xTuile, yTuile).getRevientProd().charAt(1) + "" ) );
+                    this.ctrl.getTuile(xTuile, yTuile).setActivation(false);
                 }
                 else { System.out.println("Vous avez deja était activé cette tuile !"); }
             }
@@ -221,27 +221,27 @@ public class Metier
         for (Joueur j : this.tabJoueurs)
         {
             // Affichage des infos
-            this.ctrl.getIhm().nourrirOuvrierInfo(j);
+            this.ctrl.nourrirOuvrierInfo(j);
 
             // Blé
-            iOuvrierNourri      = this.ctrl.getIhm().nourrirOuvrier ('C', iOuvrierNourri, this.NB_OUVRIERS, j);
+            iOuvrierNourri      = this.ctrl.nourrirOuvrier ('C', iOuvrierNourri, this.NB_OUVRIERS, j);
             nbRessourceConsomme = iOuvrierNourri;
             j.echangerRscJoueurVBanque(this.banque,'C',nbRessourceConsomme);
             
             // Eau
-            nbRessourceConsomme = this.ctrl.getIhm().nourrirOuvrier('E', iOuvrierNourri, this.NB_OUVRIERS, j);
+            nbRessourceConsomme = this.ctrl.nourrirOuvrier('E', iOuvrierNourri, this.NB_OUVRIERS, j);
             iOuvrierNourri      += nbRessourceConsomme;
             j.echangerRscJoueurVBanque(this.banque,'E',nbRessourceConsomme);
 
             // Pièces
-            nbRessourceConsomme = this.ctrl.getIhm().nourrirOuvrier('M', iOuvrierNourri, this.NB_OUVRIERS, j);
+            nbRessourceConsomme = this.ctrl.nourrirOuvrier('M', iOuvrierNourri, this.NB_OUVRIERS, j);
             if (nbRessourceConsomme != 0 ) nbRessourceConsomme+= 2;
 
             iOuvrierNourri      += nbRessourceConsomme;
             j.echangerRscJoueurVBanque(this.banque,'M',nbRessourceConsomme);
             
             // On affiche si le joueurs perd du score ou non.
-            this.ctrl.getIhm().finNourrir(iOuvrierNourri, this.NB_OUVRIERS, j);
+            this.ctrl.finNourrir(iOuvrierNourri, this.NB_OUVRIERS, j);
             
             // On recommence à 0 pour les autres joueurs
             iOuvrierNourri = 0;
@@ -253,11 +253,11 @@ public class Metier
     public void getInfoBatiment( )
     {
       
-       String saisie = this.ctrl.getIhm().getSaisiePos();
+       String saisie = this.ctrl.getSaisiePos();
         int y =  saisie.charAt(0) - (int) ('A');
         int x = (Integer.parseInt(saisie.charAt(1)+"")-1) ;
 
-      System.out.println(this.ctrl.getIhm().getBatiment(x,y).infoBatiment()); 
+      System.out.println(this.ctrl.getBatiment(x,y).infoBatiment()); 
     }
 
     public boolean echangerPiece()
