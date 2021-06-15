0d86777ba555ce9f8dd01dca256c1015cf4e421d
Theo <theo.lavie@live.fr>
2021-06-15 17:28:31 +0200

    Ajout des objectifs

---
+++ Code/cui/Objectif.java
@@ -0,0 +1,14 @@
+
+public class Objectif
+{
+    // Attributs
+    private String sCondition;
+    private int    iScore;
+
+
+    public Objectif(String sCondition, int iScore)
+    {
+        this.sCondition = sCondition;
+        this.iScore     = iScore;
+    }
+}
